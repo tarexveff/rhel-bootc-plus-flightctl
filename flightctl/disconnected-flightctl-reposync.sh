14873488da72c67fdc8354337016fbacaa013f91
tarexveff <55594543+tarexveff@users.noreply.github.com>
2025-06-05 10:37:25 -0400

    Update disconnected-flightctl-reposync.sh

--- flightctl/disconnected-flightctl-reposync.sh
+++ flightctl/disconnected-flightctl-reposync.sh
@@ -62,26 +62,26 @@ done
 
 
 podman image tag quay.io/keycloak/keycloak:25.0.1 $CONTAINER_REPO/keycloak/keycloak:latest
-podman image tag quay.io/openshift/origin-cli:4.20.0 $CONTAINER_REPO/openshift/origin-cli:4.20.0
+podman image tag quay.io/openshift/origin-cli:4.20.0 $CONTAINER_REPO/openshift/origin-cli:latest
 podman image tag quay.io/sclorg/postgresql-16-c9s:20250214 $CONTAINER_REPO/sclorg/postgresql-16-c9s:latest
-podman image tag docker.io/library/redis:7.4.1 $CONTAINER_REPO/library/redis:20250108
-podman image tag quay.io/flightctl/flightctl-api:0.7.1 $CONTAINER_REPO/flightctl/flightctl-api:0.7.1
-podman image tag quay.io/flightctl/flightctl-cli-artifacts:0.7.1 $CONTAINER_REPO/flightctl/flightctl-cli-artifacts:0.7.1
-podman image tag quay.io/flightctl/flightctl-periodic:0.7.1 $CONTAINER_REPO/flightctl/flightctl-periodic:0.7.1
-podman image tag quay.io/flightctl/flightctl-ui:0.7.1 $CONTAINER_REPO/flightctl/flightctl-ui:0.7.1
-podman image tag quay.io/flightctl/flightctl-worker:0.7.1 $CONTAINER_REPO/flightctl/flightctl-worker:0.7.1
+podman image tag docker.io/library/redis:7.4.1 $CONTAINER_REPO/library/redis:latest
+podman image tag quay.io/flightctl/flightctl-api:0.7.1 $CONTAINER_REPO/flightctl/flightctl-api:latest
+podman image tag quay.io/flightctl/flightctl-cli-artifacts:0.7.1 $CONTAINER_REPO/flightctl/flightctl-cli-artifacts:latest
+podman image tag quay.io/flightctl/flightctl-periodic:0.7.1 $CONTAINER_REPO/flightctl/flightctl-periodic:latest
+podman image tag quay.io/flightctl/flightctl-ui:0.7.1 $CONTAINER_REPO/flightctl/flightctl-ui:latest
+podman image tag quay.io/flightctl/flightctl-worker:0.7.1 $CONTAINER_REPO/flightctl/flightctl-worker:latest
 
 
 #Push to local registry
 podman push $CONTAINER_REPO/keycloak/keycloak:latest
-podman push $CONTAINER_REPO/openshift/origin-cli:4.20.0
+podman push $CONTAINER_REPO/openshift/origin-cli:latest
 podman push $CONTAINER_REPO/sclorg/postgresql-16-c9s:latest
-podman push $CONTAINER_REPO/library/redis:20250108
-podman push $CONTAINER_REPO/flightctl/flightctl-api:0.7.1
-podman push $CONTAINER_REPO/flightctl/flightctl-cli-artifacts:0.7.1
-podman push $CONTAINER_REPO/flightctl/flightctl-periodic:0.7.1
-podman push $CONTAINER_REPO/flightctl/flightctl-ui:0.7.1
-podman push $CONTAINER_REPO/flightctl/flightctl-worker:0.7.1
+podman push $CONTAINER_REPO/library/redis:latest
+podman push $CONTAINER_REPO/flightctl/flightctl-api:latest
+podman push $CONTAINER_REPO/flightctl/flightctl-cli-artifacts:latest
+podman push $CONTAINER_REPO/flightctl/flightctl-periodic:latest
+podman push $CONTAINER_REPO/flightctl/flightctl-ui:latest
+podman push $CONTAINER_REPO/flightctl/flightctl-worker:latest
  
 
 exit 0
