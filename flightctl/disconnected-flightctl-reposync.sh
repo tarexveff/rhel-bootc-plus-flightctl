318ba74862f54e3b8672aa3b147ca59c12577773
tarexveff <55594543+tarexveff@users.noreply.github.com>
2025-06-05 14:34:22 -0400

    Update disconnected-flightctl-reposync.sh

--- flightctl/disconnected-flightctl-reposync.sh
+++ flightctl/disconnected-flightctl-reposync.sh
@@ -10,15 +10,15 @@ export DISCONNECTED_SYSTEM=false
 
 if [ $SKOPEO == "true"]; then
 
-skopeo copy docker://quay.io/flightctl/flightctl-cli-artifacts:0.7.1 docker://$CONTAINER_REPO/flightctl/flightct-cli-artifacts:latest
-skopeo copy docker://quay.io/flightctl/flightctl-api:0.7.1 docker://$CONTAINER_REPO/flightctl/flightctl-api:latest
-skopeo copy docker://quay.io/flightctl/flightctl-periodic:0.7.1 docker://$CONTAINER_REPO/flightctl/flightctl-periodic:latest
-skopeo copy docker://quay.io/flightctl/flightctl-ui:0.7.1 docker://$CONTAINER_REPO/flightctl/flightctl-ui:latest
-skopeo copy docker://quay.io/flightctl/flightctl-worker:0.7.1 docker://$CONTAINER_REPO/flightctl/flightctl-worker:latest
-skopeo copy docker://quay.io/keycloak/keycloak:25.0.1 docker://$CONTAINER_REPO/keycloak/keycloak:latest
-skopeo copy docker://quay.io/quay.io/openshift/origin-cli:4.20.0 docker://$CONTAINER_REPO/openshift/origin-cli:latest
-skopeo copy docker://quay.io/sclorg/postgresql-16-c9s:20250214 docker://$CONTAINER_REPO/sclorg/postgresql-16-c9s:latest
-skopeo copy docker://docker.io/library/redis:7.4.1 docker://$CONTAINER_REPO/library/redis:latest
+skopeo copy --dest-tls-verify=false docker://quay.io/flightctl/flightctl-cli-artifacts:0.7.1 docker://$CONTAINER_REPO/flightctl/flightct-cli-artifacts:latest
+skopeo copy --dest-tls-verify=false docker://quay.io/flightctl/flightctl-api:0.7.1 docker://$CONTAINER_REPO/flightctl/flightctl-api:latest
+skopeo copy --dest-tls-verify=false docker://quay.io/flightctl/flightctl-periodic:0.7.1 docker://$CONTAINER_REPO/flightctl/flightctl-periodic:latest
+skopeo copy --dest-tls-verify=false docker://quay.io/flightctl/flightctl-ui:0.7.1 docker://$CONTAINER_REPO/flightctl/flightctl-ui:latest
+skopeo copy --dest-tls-verify=false docker://quay.io/flightctl/flightctl-worker:0.7.1 docker://$CONTAINER_REPO/flightctl/flightctl-worker:latest
+skopeo copy --dest-tls-verify=false docker://quay.io/keycloak/keycloak:25.0.1 docker://$CONTAINER_REPO/keycloak/keycloak:latest
+skopeo copy --dest-tls-verify=false docker://quay.io/quay.io/openshift/origin-cli:4.20.0 docker://$CONTAINER_REPO/openshift/origin-cli:latest
+skopeo copy --dest-tls-verify=false docker://quay.io/sclorg/postgresql-16-c9s:20250214 docker://$CONTAINER_REPO/sclorg/postgresql-16-c9s:latest
+skopeo copy --dest-tls-verify=false docker://docker.io/library/redis:7.4.1 docker://$CONTAINER_REPO/library/redis:latest
 exit 0
 fi
 
