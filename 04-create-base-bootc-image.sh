6247b15cd2574804ccfa9b38fb068df647c06438
tarexveff <55594543+tarexveff@users.noreply.github.com>
2025-08-21 08:38:09 -0400

    Update 04-create-base-bootc-image.sh

--- 04-create-base-bootc-image.sh
+++ 04-create-base-bootc-image.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ ! -f ~/.ssh/id_core ]; then
-    ssh-keygen -t rsa -f ~/.ssh/id_core
-fi
+#if [ ! -f ~/.ssh/id_core ]; then
+#    ssh-keygen -t rsa -f ~/.ssh/id_core
+#fi
 
-. $(dirname $0)/demo.conf
+. $(dirname $0)/env.conf
 
 podman login registry.redhat.io
 podman pull registry.redhat.io/rhel9/rhel-bootc:9.6
